6ca76ca7312fb02a9785b4b7cefa1d9ddd1f1e97
Chris <clenart1@icloud.com>
2012-11-07 17:24:29 -0600

    Switched tabs to spaces in sublime...

--- lib/test.rb
+++ lib/test.rb
@@ -33,7 +33,7 @@ else
 	puts "get_topic_list test passed"
 end
 
-=begin
+
 # post_topic test
 begin
 	eti.post_topic "tnwefg", "im gay"
@@ -56,11 +56,10 @@ end
 
 # get_topic_by_id test
 begin
-	eti.get_topic_by_id 8176561
+	puts eti.get_topic_by_id 8176561
 rescue ETIError => e
 	puts "get_topic_by_id test failed: "
 	puts e.message
 else
 	puts "get_topic_by_id passed"
 end
-=end
